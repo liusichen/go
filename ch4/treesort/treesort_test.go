d090eb1ff08f56366b66556c3fa71ebe1403c934
Sichen Liu <guijiao1234@gmail.com>
2017-05-09 17:17:59 +0800

    遍历值

---
+++ ch4/treesort/treesort_test.go
@@ -0,0 +1,19 @@
+package sort
+
+import (
+	"learn/ch4/treesort"
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	data := make([]int, 50)
+	for i := range data {
+		data[i] = rand.Int() % 50
+	}
+	treesort.Sort(data)
+	if !sort.IntsAreSorted(data) {
+		t.Errof("not sorted : %v\n", data)
+	}
+}
