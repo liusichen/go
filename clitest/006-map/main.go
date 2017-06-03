6318f88e00eb8eb5c1d5f65819e11d743b74f879
Sichen Liu <guijiao1234@gmail.com>
2017-06-03 10:46:48 +0800

    map使用，＝在key与value之间

---
+++ clitest/006-map/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/mkideal/cli"
+)
+
+type argT struct {
+	cli.Helper
+	Macros map[string]int `cli:"D" usage:"define macros"`
+}
+
+func main() {
+	cli.Run(new(argT), func(ctx *cli.Context) error {
+		ctx.JSONln(ctx.Argv())
+		return nil
+	})
+}
